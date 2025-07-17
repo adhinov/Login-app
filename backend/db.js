069ed508dd1a5288b9907e346f50274cf6716d52
Adhinov <133613543+adhinov@users.noreply.github.com>
2025-07-17 13:29:40 +0700

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
-// db.js
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createConnection({
@@ -12,10 +11,10 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error('❌ Database connection failed:', err);
-    return;
+    console.error("❌ Database connection failed:", err);
+  } else {
+    console.log("✅ Connected to MySQL database");
   }
-  console.log('✅ Connected to MySQL database');
 });
 
 module.exports = db;
