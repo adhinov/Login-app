bba8ac40a3897ba9cf27d55d2a3ebcf1d853019c
adhinov <adhinov4@gmail.com>
2025-09-09 10:46:06 +0700

    fix adminDashboard 5

--- frontend/src/components/AdminDashboard.tsx
+++ frontend/src/components/AdminDashboard.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./global.css";
 
 // Antarmuka untuk data pengguna, dengan properti opsional
 interface User {
